218f25406b5256d404447401c5393a8aa017f8b4
cullan-liang <cullan.liang@hotmail.com>
2016-05-17 10:29:22 -0400

    Changed Rmd Knitr Script

--- knitToRm.R
+++ knitToRm.R
@@ -1,8 +1,7 @@
+# SETUP: Load the knitr library
 library(knitr)
 
-# Set current directory
-# Session -> Set Working Directory -> To Source File Location
-
+# SETUP: Load the knit.rmd.file function
 knit.rmd.file <- function(input.path, output.path){
   VAR <- ""
   knitr::knit(input=input.path, output=output.path)
@@ -13,19 +12,33 @@ knit.rmd.file <- function(input.path, output.path){
 }
 
 # 1. Change knit function input directory to select the location of the RMarkdown file ("RMD/xxxxx/Name.Rmd")
-# 2. Change knit function output directory of the Markdown file ("/_posts/YYYY-MM-DD-Name.md")
+# %%% Example: knit.rmd.file("RMD/FAA-Strategy/FAA_Explanation.Rmd", .....)
+
+# 2. Change knit function output directory of the Markdown file ("_posts/YYYY-MM-DD-Name.md")
+# %%% Example: knit.rmd.file(....., "_posts/2016-05-16-FAA_Explanation.md")
+
 # 3. Within the .Rmd file, change the fig.path to paste0(VAR, 'public/images/YYYY-MM-DD-Name/')
 # ***Note for 3. fig.path is an attribute within opts_chunk$set(fig.path=..., message=..., warning=..., echo=...)***
+# %%% Example: opts_chunk$set(fig.path=paste0(VAR, 'public/images/2014-10-27-DE_and_Reverse/'), .....)
+
 # 4. Add the following to the TOP of the .Rmd file: ***NEED THE LAYOUT: POST***
 # ---
 #  layout: post
-#  title: "Name"
+#  title: "Name of Post"
 # ---
+# %%% Example:
+# %%% ---
+# %%%  layout: post
+# %%%  title: "DE and Reverse DE"
+# %%% ---
+
+# 5. Run the knit.rmd.file function (Examples shown below)
 
 knit.rmd.file("RMD/FAA-Strategy/FAA_Explanation.Rmd", "_posts/2016-05-16-FAA_Explanation.md")
 knit.rmd.file("RMD/Markowitz - Ilya/2015_06_05_Markowitz.Rmd", "_posts/2015-06-05-Markowitz.md")
 knit.rmd.file("RMD/Rotation/Rotation_Strategy.Rmd", "_posts/2015-05-18-Rotation_Strategy.md")
 knit.rmd.file("RMD/Momentum Markowitz - SIT/Momentum_Markowitz_Post.Rmd", "_posts/2015-06-04-Momentum_Markowitz.md")
 knit.rmd.file("RMD/COGS_to_Revenue/COGS_to_Revenue.Rmd", "_posts/2016-05-16-COGS_to_Revenue.md")
+knit.rmd.file("RMD/DE_and_Reverse/DE_and_Reverse_DE.Rmd", "_posts/2014-10-27-DE_and_Reverse.md")
 
-
+# 6. Push the changes to git
